ae35bf0d069c29be8d81bd529acb69d425e04d70
GustGomes <97971803+GustGomes@users.noreply.github.com>
2023-11-16 15:02:04 -0300

    Add files via upload

---
+++ rpachal1.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import rpa as r
+
+#leitura e captura dos dados
+df = pd.read_excel(r"C:\Users\gusta\PROJETOS\pyprojects\rpa\challenge.xlsx")
+# verifica os dados
+# print(df)
+r.init(turbo_mode=True)
+
+#abre nav
+r.url('http://www.rpachallenge.com/')
+r.wait(5)
+#começa o desafio
+r.click('/html/body/app-root/div[2]/app-rpa1/div/div[1]/div[6]/button')
+
+for index,row in df.iterrows():
+    r.type('//input[@ng-reflect-name="labelFirstName"]',row['First Name'])
+    r.type('//input[@ng-reflect-name="labelLastName"]',row['Last Name '])
+    r.type('//input[@ng-reflect-name="labelCompanyName"]',row['Company Name'])
+    r.type('//input[@ng-reflect-name="labelRole"]',row['Role in Company'])
+    r.type('//input[@ng-reflect-name="labelAddress"]',row['Address'])
+    r.type('//input[@ng-reflect-name="labelEmail"]',row['Email'])
+    r.type('//input[@ng-reflect-name="labelPhone"]',str(row['Phone Number']))
+    r.click('//input[@value="Submit"]')
+
+
+#fecha o processo 
+#r.close()
