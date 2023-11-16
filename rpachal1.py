17af66d060c8a81f10babf197abb871a40c27acf
GustGomes <97971803+GustGomes@users.noreply.github.com>
2023-11-16 15:05:56 -0300

    rpachal1.py

--- rpachal1.py
+++ rpachal1.py
@@ -12,7 +12,7 @@ r.url('http://www.rpachallenge.com/')
 r.wait(5)
 #começa o desafio
 r.click('/html/body/app-root/div[2]/app-rpa1/div/div[1]/div[6]/button')
-
+#insere os dados nos campos
 for index,row in df.iterrows():
     r.type('//input[@ng-reflect-name="labelFirstName"]',row['First Name'])
     r.type('//input[@ng-reflect-name="labelLastName"]',row['Last Name '])
@@ -23,6 +23,7 @@ for index,row in df.iterrows():
     r.type('//input[@ng-reflect-name="labelPhone"]',str(row['Phone Number']))
     r.click('//input[@value="Submit"]')
 
-
+#Print da pagina
+#r.snap('/html/body/app-root/div[2]','resultado.png')
 #fecha o processo 
 #r.close()
